5afc32b5e79fd026f14a99f636a0d76a0f7faf02
stoneYuan3 <jackyuan110@gmail.com>
2023-03-27 15:52:28 -0700

    add poster 112-03-27

--- src/pages/general.js
+++ src/pages/general.js
@@ -2,7 +2,13 @@
 export const Home = () => {
     return (
       <div className="flex flex-column section_article">
-        <p className="p_intro">A tabletop game build for Spring 2023 IAT312 Game Development class, part of the fabulous Simon Fraser University School of Interactive Art and Technology.</p>
+        <p className="p_intro">A tabletop game build for Spring 2023 IAT312 Foundations of Game Design, part of the fabulous Simon Fraser University School of Interactive Arts and Technology.</p>
+
+        <figure id="poster">
+            <img src="/img/poster.jpg" alt="game poster" />
+            <figcaption>Poster!</figcaption>
+        </figure>
+
         <p>More Contents Coming!</p>
       </div>
     );
